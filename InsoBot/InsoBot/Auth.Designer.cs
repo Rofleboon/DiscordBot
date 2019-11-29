e33e2053a1bc1c217dbff30ae68f84d358d5659a
Martin Linder <martin.linder@bite.de>
2019-11-29 14:49:21 +0100

    Added restsharp oauth2 auth first steps

---
+++ InsoBot/InsoBot/Auth.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace InsoBot
+{
+    partial class Auth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.linkDiscordAcc = new System.Windows.Forms.LinkLabel();
+            this.btnDiscordAuth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // linkDiscordAcc
+            // 
+            this.linkDiscordAcc.AutoSize = true;
+            this.linkDiscordAcc.Location = new System.Drawing.Point(39, 397);
+            this.linkDiscordAcc.Name = "linkDiscordAcc";
+            this.linkDiscordAcc.Size = new System.Drawing.Size(132, 13);
+            this.linkDiscordAcc.TabIndex = 0;
+            this.linkDiscordAcc.TabStop = true;
+            this.linkDiscordAcc.Text = "Link your Discord Account";
+            this.linkDiscordAcc.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkDiscordAcc_LinkClicked);
+            // 
+            // btnDiscordAuth
+            // 
+            this.btnDiscordAuth.Location = new System.Drawing.Point(276, 155);
+            this.btnDiscordAuth.Name = "btnDiscordAuth";
+            this.btnDiscordAuth.Size = new System.Drawing.Size(167, 97);
+            this.btnDiscordAuth.TabIndex = 1;
+            this.btnDiscordAuth.Text = "button1";
+            this.btnDiscordAuth.UseVisualStyleBackColor = true;
+            this.btnDiscordAuth.Click += new System.EventHandler(this.btnDiscordAuth_Click);
+            // 
+            // Auth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDiscordAuth);
+            this.Controls.Add(this.linkDiscordAcc);
+            this.Name = "Auth";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkDiscordAcc;
+        private System.Windows.Forms.Button btnDiscordAuth;
+    }
+}
+
---
+++ InsoBot/InsoBot/Auth.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Discord;
+using Discord.WebSocket;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace InsoBot
+{
+    public partial class Auth : Form
+    {
+
+
+        public Auth()
+        {
+            InitializeComponent();
+        }
+
+        public static int GetRandomUnusedPort()
+        {
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        }
+
+
+
+        private DiscordSocketClient _client;
+        private string authLink = "https://discordapp.com/api/oauth2/authorize?client_id=649900073286631434&redirect_uri=https%3A%2F%2Fdepleto.com%2Fredirect%2F&response_type=code&scope=webhook.incoming%20messages.read";
+
+
+
+        public async Task Test ()
+        {
+            _client = new DiscordSocketClient();
+            await _client.LoginAsync(TokenType.Webhook, "577fctqeaG31QU3OFluR9SB2LNKRhsEP");
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private void linkDiscordAcc_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(authLink);
+        }
+
+        private void btnDiscordAuth_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://discordapp.com/api/oauth2/token");
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("grant_type", "client_credentials");
+            request.AddHeader("Connection", "keep-alive");    
+            request.AddHeader("Accept-Encoding", "gzip, deflate");
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            client.Authenticator = new HttpBasicAuthenticator("649900073286631434", "577fctqeaG31QU3OFluR9SB2LNKRhsEP");
+            IRestResponse response = client.Execute(request);
+        }
+    }
+}
